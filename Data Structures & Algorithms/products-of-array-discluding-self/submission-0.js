b2253900554aeaba22af6d3ff7c8f09c7bca9694
Gbolahan Olanrewaju <olanrewajugbolahan226@gmail.com>
2026-05-06 21:44:07 +0100

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const n = nums.length;
+        const output = new Array(n);
+
+        let leftProduct = 1;
+        for (let i = 0; i < n; i++) {
+            output[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        let rightProduct = 1;
+        for (let i = n - 1; i >= 0; i--) {
+            output[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return output;
+    }
+}
