632c055940662e3e0b8c86f06a0681bd982848e2
Gbolahan Olanrewaju <olanrewajugbolahan226@gmail.com>
2026-05-22 12:29:35 +0100

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let hashSet = new Set(); // check if the character is present in the window or not
+        let leftBoundary = 0;
+        let maxLength = 0;
+
+        for (let rightBoundary = 0; rightBoundary < s.length; rightBoundary++) {
+            while (hashSet.has(s[rightBoundary])) {
+                hashSet.delete(s[leftBoundary]);
+                leftBoundary++;
+            }
+            hashSet.add(s[rightBoundary]);
+            maxLength = Math.max(maxLength, rightBoundary - leftBoundary + 1);
+        }
+
+        return maxLength;
+    }
+}
