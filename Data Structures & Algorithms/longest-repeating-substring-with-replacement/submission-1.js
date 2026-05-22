16649f64cfd29b4c5c3ed73e7b8e0fb5277c2c74
Gbolahan Olanrewaju <olanrewajugbolahan226@gmail.com>
2026-05-22 12:29:07 +0100

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        if (s.length === 0) return 0;
+
+        let left = 0;
+        let maxWindowSize = 0;
+
+        let hashTable = {};
+        let maxFrequency = 0;
+
+
+        for (let right = 0; right < s.length; right++) {
+            const char = s[right];
+            hashTable[char] = (hashTable[char] || 0) + 1;
+
+            if (hashTable[char] > maxFrequency) {
+                maxFrequency = hashTable[char];
+            }
+
+            if ((right - left + 1) - maxFrequency > k) {
+                hashTable[s[left]] = hashTable[s[left]] - 1;
+                left++;
+            }
+            maxWindowSize = Math.max(maxWindowSize, right - left + 1)
+        }
+        return maxWindowSize;
+    }
+}
+
+
+
